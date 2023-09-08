eded638a094ce5485eb9de5bca89e54b5bf3e976
spurs0929 <briankang0929@gmail.com>
2023-09-08 23:01:32 +0800

    新增公共方法

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,12 @@
+// 淺拷貝
 export const extend = Object.assign;
 
+// 判斷傳入值的型別是否為object
 export const isObject = (val) => {
   return val !== null && typeof val === "object";
+};
+
+// 判斷傳入的兩個值是否相等
+export const hasChanged = (value, newValue) => {
+  return !Object.is(value, newValue);
 };
