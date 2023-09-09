792675d787e8bfcbf419e46c994894eaae46a1c8
spurs0929 <briankang0929@gmail.com>
2023-09-09 22:56:22 +0800

    ref 功能新增測試

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref } from "../ref";
 
 describe("ref", () => {
   it("happy pass", () => {
@@ -37,5 +38,15 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  it("isRef", () => {
+    const a = ref(1);
+    const b = reactive({
+      c: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(b)).toBe(false);
   });
 });
