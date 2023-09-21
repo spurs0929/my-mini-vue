7b78037353574fbf2e67bfd1b3fc3724e0a0e0c4
spurs0929 <briankang0929@gmail.com>
2023-09-21 22:04:43 +0800

    實現 shapeFlags

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstanse, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -9,9 +10,10 @@ function patch(vnode, container) {
   // 處理組件
   // 判斷vnode是不是element
   // processElement();
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 }
@@ -23,10 +25,10 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
 
-  const { children, props } = vnode;
-  if (typeof children === "string") {
+  const { children, props, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, container);
   }
 
