b05f4f86f3baf21ca222341b38c789d474abb741
spurs0929 <briankang0929@gmail.com>
2023-10-02 21:38:45 +0800

    實現作用域插槽功能

--- example/componentSlot/Foo.js
+++ example/componentSlot/Foo.js
@@ -6,9 +6,9 @@ export const Foo = {
   },
   render() {
     const foo = h("p", {}, "foo");
-    console.log(this.$slots);
+    const age = 18;
     return h("div", {}, [
-      renderSlots(this.$slots, "header"),
+      renderSlots(this.$slots, "header", { age }),
       foo,
       renderSlots(this.$slots, "footer"),
     ]);
--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -5,7 +5,7 @@ export function initSlots(instance, children) {
 function normalizeObjectSlots(children: any, slots: any) {
   for (const key in children) {
     const value = children[key];
-    slots[key] = normalizeSlotValue(value);
+    slots[key] = (props) => normalizeSlotValue(value(props));
   }
 }
 
